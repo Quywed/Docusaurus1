889ea45419f540d74ee7157f0f03cb4a2ddfb8d8
Lucas Martins <a79294@ualg.pt>
2023-12-04 15:46:15 +0000

    Retirei

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,7 +14,8 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Teste teste
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
       </>
     ),
   },
