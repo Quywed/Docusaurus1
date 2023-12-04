09599d6c91920d8c6e6a5d275621b810b0f9d5a3
Lucas Martins <a79294@ualg.pt>
2023-12-04 16:02:08 +0000

    Retrocedi as alteracoes do ultimo commit

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,7 +14,8 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-       OLÁ TESTE TESTE
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
       </>
     ),
   },
