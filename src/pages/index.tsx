98b0df4e6913ede959f7394b83a4f222ff580f67
Lucas Martins <a79294@ualg.pt>
2023-12-16 20:17:38 +0000

    Mudei o banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,12 @@ function HomepageHeader() {
       <div className="container">
         <div className={styles.bannerContent}>
           <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <p
+            className="hero__subtitle"
+            style={{ color: 'black' }} // Set the color to black
+          >
+            {siteConfig.tagline}
+          </p>
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
