88b0268c4175652bbef39abef371f124e8e66ec2
Lucas Martins <a79294@ualg.pt>
2023-12-16 19:58:20 +0000

    Mudei o banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,14 @@ import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const carImageURL =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Photographer.jpg/800px-Photographer.jpg?20070216144122'; // Replace with an appropriate car workshop image
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header
+      className={clsx('hero hero--primary', styles.heroBanner)}
+      style={{ backgroundImage: `url(${carImageURL})` }}
+    >
       <div className="container">
         <div className={styles.bannerContent}>
           <h1 className="hero__title">{siteConfig.title}</h1>
@@ -18,7 +24,8 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
-              to="/docs/intro">
+              to="/docs/intro"
+            >
               Docusaurus Tutorial - 5min ⏱️
             </Link>
           </div>
@@ -27,6 +34,7 @@ function HomepageHeader() {
     </header>
   );
 }
+
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
