b43b437593bb1da3c7339dadf944e7a865af8065
Lucas Martins <a79294@ualg.pt>
2023-12-16 19:32:08 +0000

    Tentei adicionar imagem ao header da pagina principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,20 +12,26 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div className={styles.bannerContent}>
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro">
+              Docusaurus Tutorial - 5min ⏱️
+            </Link>
+          </div>
         </div>
+        <img
+          className={styles.bannerImage}
+          src="/img/home_header.png" // Replace with your image path
+          alt="Banner Image"
+        />
       </div>
     </header>
   );
 }
-
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
