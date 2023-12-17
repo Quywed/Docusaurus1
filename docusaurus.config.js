96f3ffa4730cb0cbf0b4021719ac0ae04250a227
Lucas Martins <a79294@ualg.pt>
2023-12-17 01:44:51 +0000

    Teste header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,6 +78,7 @@ const config = {
             label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/services', label: 'Serviços', position: 'left'},
         ],
       },
       footer: {
