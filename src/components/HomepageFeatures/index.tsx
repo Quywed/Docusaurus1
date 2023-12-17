ebff0b66e1ddf913777749b65a664926b9374958
Lucas Martins <a79294@ualg.pt>
2023-12-17 16:56:39 +0000

    Alteração no banner

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+
 import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
+  link?: string; // Add a link property to FeatureItem
 };
 
 const FeatureList: FeatureItem[] = [
@@ -14,7 +17,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/car_oil.svg').default,
     description: (
       <>
-        Fazemos a mudança de óleo na compra de serviços superior acima de 10€. Garantidadmente que não é só água...
+        Fazemos a mudança de óleo na compra de serviços superior acima de 10€. Garantidamente que não é só água...
       </>
     ),
   },
@@ -32,13 +35,23 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/bike-14.svg').default,
     description: (
       <>
-        Oferecemos servico de arranjo a bicicletas de criancas menores de 30 anos.
+        Oferecemos serviço de arranjo a bicicletas de crianças menores de 30 anos.
       </>
     ),
   },
+  {
+    title: 'Serviços',
+    Svg: require('@site/static/img/bike-14.svg').default,
+    description: (
+      <>
+        Clique <Link to="/docs/service">neste link</Link> para aceder ao conjunto de serviços que disponibilizamos.
+      </>
+    ),
+    link: '/docs/service', // Specify the link for the "Serviços" feature
+  },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description, link }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -46,7 +59,7 @@ function Feature({title, Svg, description}: FeatureItem) {
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p>{link ? <Link to={link}>{description}</Link> : description}</p>
       </div>
     </div>
   );
