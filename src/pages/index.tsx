491cc499b21435bb0066eabeddf615abe2c52f8b
Lucas Martins <a79294@ualg.pt>
2023-12-17 00:36:57 +0000

    Mudei a cor das letras

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,12 +35,6 @@ function HomepageHeader() {
             {siteConfig.tagline}
           </p>
           <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs/intro"
-            >
-              Docusaurus Tutorial - 5min ⏱️
-            </Link>
           </div>
         </div>
       </div>
